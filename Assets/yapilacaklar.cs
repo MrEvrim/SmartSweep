5b1c790c49dfc315dab0693737bdbf0ccd26a005
nasoki <will.98.black@hotmail.com>
2024-03-19 17:16:35 +0300

    home-level

--- Assets/yapilacaklar.cs
+++ Assets/yapilacaklar.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class yapilacaklar : MonoBehaviour
 {
-    //ev sahnesi için ---> çöpler, ui, cutscene
+    //ev sahnesi için ---> çöpler, ui, cutscene ---- done done and done baby!
+    //flashý yedikten sonra ne olacaðýna karar vermeliyiz, merdivenlerden düþme animasyonu yapýlacak, alt kata anahtar - all done
+    //ai sesi eklenecek, müzik -maybe- , menü yapýlacak (how to play yapmayý unutma ve ses kontrol paneli -maybe-)
 }
