ba19454a15e337c1fe9c45d7aaac029cb0c3849e
Evrim <149576486+MrEvrim@users.noreply.github.com>
2024-03-22 23:36:02 +0300

    Drone & Pickupble items
    Helüüüü

--- Assets/Scripts/Drone/DroneCont.cs
+++ Assets/Scripts/Drone/DroneCont.cs
@@ -1,15 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DroneCont : MonoBehaviour
 {
-    void Start()
+    Rigidbody rb;
+    float udAxis, fbAxis, rlAxis;
+    float fbAngle = 0, rlAngle = 0;
+    [SerializeField]
+    float speed = 1.3f, angle = 25;
+    bool isOnGround = false;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-    void Update()
+    private void Update()
     {
-        
+        Controlls();
+        transform.localEulerAngles = Vector3.back * rlAngle + Vector3.right * fbAngle;
+    }
+    private void FixedUpdate()
+    {
+        rb.AddRelativeForce(rlAxis, udAxis, fbAxis);
+    }
+    void Controlls()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            udAxis = 10 * speed;
+            isOnGround = false;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            udAxis = 8;
+            isOnGround = false;
+        }
+        else
+            udAxis = 9.81f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            fbAngle = Mathf.Lerp(fbAngle, angle, Time.deltaTime);
+            fbAxis = speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            fbAngle = Mathf.Lerp(fbAngle, -angle, Time.deltaTime);
+            fbAxis = -speed;
+        }
+        else
+        {
+            fbAngle = Mathf.Lerp(fbAngle, 0, Time.deltaTime);
+            fbAxis = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rlAngle = Mathf.Lerp(rlAngle, angle, Time.deltaTime);
+            rlAxis = speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rlAngle = Mathf.Lerp(rlAngle, -angle, Time.deltaTime);
+            rlAxis = -speed;
+        }
+        else
+        {
+            rlAngle = Mathf.Lerp(rlAngle, 0, Time.deltaTime);
+            rlAxis = 0;
+        }
+
+        if ((Input.GetKey(KeyCode.W)) && (Input.GetKey(KeyCode.D)))
+        {
+            fbAngle = Mathf.Lerp(fbAngle, angle, Time.deltaTime);
+            rlAngle = Mathf.Lerp(rlAngle, angle, Time.deltaTime);
+            fbAxis = 0.5f * speed;
+            rlAxis = 0.5f * speed;
+        }
+
+        if ((Input.GetKey(KeyCode.W)) && (Input.GetKey(KeyCode.A)))
+        {
+            fbAngle = Mathf.Lerp(fbAngle, angle, Time.deltaTime);
+            rlAngle = Mathf.Lerp(rlAngle, -angle, Time.deltaTime);
+            fbAxis = 0.5f * speed;
+            rlAxis = 0.5f * speed;
+        }
+        if ((Input.GetKey(KeyCode.S)) && (Input.GetKey(KeyCode.D)))
+        {
+            fbAngle = Mathf.Lerp(fbAngle, -angle, Time.deltaTime);
+            rlAngle = Mathf.Lerp(rlAngle, angle, Time.deltaTime);
+            fbAxis = -0.5f * speed;
+            rlAxis = -0.5f * speed;
+        }
+        if ((Input.GetKey(KeyCode.S)) && (Input.GetKey(KeyCode.D)))
+        {
+            fbAngle = Mathf.Lerp(fbAngle, -angle, Time.deltaTime);
+            rlAngle = Mathf.Lerp(rlAngle, -angle, Time.deltaTime);
+            fbAxis = -0.5f * speed;
+            rlAxis = -0.5f * speed;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            isOnGround = true;
     }
 }
---
+++ Assets/Scripts/Drone/PickupObject.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class PickupObject : MonoBehaviour
+{
+    public Transform pickupDestination;
+    public TMP_Text bilgiTxt;
+
+    private GameObject currentPickup;
+    private bool canDrop = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (currentPickup != null && !canDrop)
+            {
+                StartCoroutine(DropObjectRoutine());
+                bilgiTxt.text = "";
+            }
+        }
+
+        if (canDrop && currentPickup != null)
+        {
+            DropObject();
+        }
+    }
+
+    IEnumerator DropObjectRoutine()
+    {
+        canDrop = true;
+        yield return new WaitForSeconds(3);
+        canDrop = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickupable") && currentPickup == null)
+        {
+            // Objeyi al
+            PickupObjectMethod(other.gameObject);
+            bilgiTxt.text = "Gıda Kolisini bırakmak için 'R'ye bas.";
+        }
+    }
+
+    private void PickupObjectMethod(GameObject pickup)
+    {
+        // Şu anki taşınan objeyi pickle
+        currentPickup = pickup;
+        currentPickup.transform.SetParent(pickupDestination);
+        currentPickup.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void DropObject()
+    {
+        // Eğer bir şey taşıyorsa
+        currentPickup.GetComponent<Rigidbody>().isKinematic = false;
+        currentPickup.transform.SetParent(null);
+        currentPickup = null;
+        bilgiTxt.text = "";
+    }
+}
